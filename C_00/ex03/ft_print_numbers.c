600c6a3e77ca3f7ca1282d3750ab6fd0c9f151bf
shadythegiant <hellgatesdesigner2010@gmail.com>
2025-08-13 20:10:16 +0100

    :octocat:

---
+++ C_00/ex03/ft_print_numbers.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+void ft_putchar(char c) { 
+    write(1, &c, 1); 
+
+}
+
+void ft_print_numbers(void) { 
+    int i = 48; 
+    while (i <= 57) { 
+        ft_putchar(i); 
+        i++; 
+    }
+}
+
+int main(void) {
+    ft_print_numbers(); 
+    return 0;
+}
