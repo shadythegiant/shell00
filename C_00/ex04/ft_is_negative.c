5f3cab466df3c9c936ee866c91867edec78e556f
shadythegiant <hellgatesdesigner2010@gmail.com>
2025-08-17 00:01:25 +0100

    awddi

--- C_00/ex04/ft_is_negative.c
+++ C_00/ex04/ft_is_negative.c
@@ -3,16 +3,16 @@
 void	ft_is_negative(int n)
 {
 	char	p;
-	char	n;
+	char	c;
 
 	p = 'P';
-	n = 'N';
+	c = 'N';
 	if (n > 0)
 	{
 		write(1, &p, 1);
 	}
 	if (n < 0)
 	{
-		write(1, &n, 1);
+		write(1, &c, 1);
 	}
 }
